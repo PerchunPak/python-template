ddc30aaed8a8e564cba4084fc688ef0c46a31ad0
PerchunPak <68118654+PerchunPak@users.noreply.github.com>
2022-04-13 21:15:57 +0300

    Fix remove_trash script in docs/

--- {{ cookiecutter.project_name }}/docs/remove_trash.py
+++ {{ cookiecutter.project_name }}/docs/remove_trash.py
@@ -5,7 +5,7 @@ from os import remove
 
 def remove_trash():
     """Remove residual trash generated with `sphinx`."""
-    for file in glob("modules/autodonate*.rst"):
+    for file in glob("modules/{{ cookiecutter.project_name }}*.rst"):
         remove(file)
 
 
