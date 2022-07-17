5f057680b972c9e56b64e8e99912189f05106fc7
PerchunPak <perchunpak@gmail.com>
2022-07-17 14:55:02 +0200

    Commit changes in CI with GitHub API
    Now commits will be with `verified` label!

--- tests/test_none.py
+++
@@ -1,8 +0,0 @@
-"""Created for fix crash mypy with:
-"There are no .py[i] files in directory 'tests'".
-"""
-
-
-def test_always_true() -> None:
-    """Just placeholder for pytest work without tests."""
-    assert True
