28f540642d03e3a4370a3a93a749a94996e1e3ed
PerchunPak <perchunpak@gmail.com>
2022-10-26 12:44:49 +0200

    Remove unused custom styles in docs

--- {{ cookiecutter.project_name }}/docs/conf.py
+++ {{ cookiecutter.project_name }}/docs/conf.py
@@ -98,9 +98,6 @@ set_type_checking_flag = True
 # Automatically generate section labels:
 autosectionlabel_prefix_document = True
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 
@@ -147,11 +144,6 @@ html_theme = "furo"
 html_theme_options = {
     "navigation_with_keys": True,
 }
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
 
 # -- Extension configuration -------------------------------------------------
 
